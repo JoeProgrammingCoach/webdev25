e35edc146f395786ecee47db0eddc2141fae4185
unknown <jcorndog1@ymail.com>
2025-02-03 21:51:40 -0500

    Made the searched for city appear for 5 day forecast

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const searchBtn = document.querySelector('submit');
-document.getElementById("submit").onclick = () => {
+let searchquery = "";
+let hasSearched = false;
+
+document.getElementById("submit").onclick = (e) => {
+   e.preventDefault();
+   let searchForCity =
+      document.getElementById('cityName').value;
    console.log("submit button was clicked");
+   console.log("You want weather for " + searchForCity);
+   let forecastCityName = document.getElementById("forecastCityName");
+   hasSearched = true;
+   forecastCityName.innerText = searchForCity;
 }
+
+let fTempChecked = document.getElementById("Fahrenheit");
+let cTempChecked = document.getElementById("Celsius");
 
