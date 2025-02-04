37eba0e4c7513a53fe369185930185915331bb37
unknown <jcorndog1@ymail.com>
2025-02-04 10:14:44 -0500

    finally getting temperature data!

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const searchBtn = document.querySelector('submit');
 let searchquery = "";
 let hasSearched = false;
+let currentWeatherBox = document.getElementById('currentWeather');
 let forecastCityName = document.getElementById("forecastCityName");
+let unit = "imperial";
+let searchForCity;
 
 document.getElementById("submit").onclick = (e) => {
    e.preventDefault();
-   let searchForCity =
+   searchForCity =
       document.getElementById('cityName').value;
    console.log("submit button was clicked");
    console.log("You want weather for " + searchForCity);
    hasSearched = true;
    forecastCityName.innerText = searchForCity;
+   fetchWeatherData();
 }
 
 let fTempChecked = document.getElementById("Fahrenheit");
@@ -18,5 +22,28 @@ let cTempChecked = document.getElementById("Celsius");
 
 function fetchWeatherData() {
    const apiKey = '7dc4703261b08dae5a9e117c8eed5831';
-   const fiveDayForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=${unit}&appid=${apiKey}`;
+   const fiveDayForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchForCity}&units=${unit}&appid=${apiKey}`;
+
+   fetch(fiveDayForecastUrl)
+      .then(response => response.json())
+      .then(forecastData => {
+         // Find today's forecast (assuming it's the first item in the list)
+         //const currentDayForecast = forecastData.list[0];
+         console.log(forecastData);
+
+
+         const forecastItems = forecastData.list
+            .filter((_, index) => index % 8 === 0) // Filter daily forecasts
+            .map((item, index) => ({
+               _id: `day-${index}`,
+               date: new Date(item.dt * 1000).toLocaleDateString(),
+               temp: `${Math.round(item.main.temp)}`,
+               icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
+               description: item.weather[0].description,
+
+            }));
+         // currentWeatherBox.innerText = 
+
+         console.log("data in forecast Items " + JSON.stringify(forecastItems))
+      })
 }
